83c84bfbdb42269baccaa6d05c3b68d7ab4c6a52
Adam Romanski <adamromanski@MacBook-Pro-Adam.local>
2021-03-21 21:36:59 +0100

    Create news section
    (cherry picked from commit ef64a7127d2e3edd93e28d4f91d754f11c3aba5d)

--- src/components/templates/MainTemplate.js
+++ src/components/templates/MainTemplate.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import Navigation from 'components/organisms/Navigation/Navigation';
+import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
+import { SearchBar } from 'components/organisms/SearchBar/SearchBar';
+import NewsSection from 'components/templates/NewsSection/NewsSection';
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  /* overflow-x: scroll;
-  display: grid;
-  grid-template-columns: 150px 1fr 0.75fr;
-  grid-template-rows: 90px; */
-`;
 const MainTemplate = ({ children }) => {
   return (
     <Wrapper>
       <Navigation />
+      <SearchBar />
       {children}
+      <NewsSection />
     </Wrapper>
   );
 };
